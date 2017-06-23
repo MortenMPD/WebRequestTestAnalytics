7e5e87fde6919ed76b36bc5e02020dafc0238ad9
MPD <MPD@DK01WS1691>
2017-06-23 10:46:15 +0200

    Checking in lates changes.

--- WebRequestTestAnalytics/Program.cs
+++ WebRequestTestAnalytics/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WebRequestTestAnalytics {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            var apiAddress =
+                "http://envtools/Analytics/Grids?date=2017-05-25&todate=2017-06-23&filterteam=MO-MO-T,MO-PEM-T,MO-RIM-T&grouping=testdata,testcase&measure=results,rerunned,passed,failed,analysed,defects,falsepositives,notanalysed,passedpct,failedpct,defectspct,analysedpct,falsepositivespct,notanalysedpct";
+            Uri uri = new Uri(apiAddress);
+            ICredentials credentials = CredentialCache.DefaultCredentials;
+            NetworkCredential credential = credentials.GetCredential(uri, "Basic");
+            WebReader webReader = new WebReader(apiAddress, credential);
+            var texts = webReader.GetData();
+            foreach (string text in texts)
+            {
+                System.Console.WriteLine(text);
+            }
+            System.Console.Read();
         }
     }
 }
---
+++ WebRequestTestAnalytics/WebReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace WebRequestTestAnalytics {
+    using System.Net;
+    using System.Collections.Generic; 
+    using System.IO; 
+    using System.Xml; 
+    
+    class WebReader
+    {
+        private string _apiAddress;
+        private NetworkCredential _credentials;
+
+        public WebReader(string apiAddress, NetworkCredential credentials)
+        {
+            _apiAddress = apiAddress;
+            _credentials = credentials;
+        }
+
+        public IEnumerable<string> GetData()
+         { 
+             List<string> resultList = new List<string>(); 
+             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this._apiAddress); 
+             request.Method = "GET";
+             request.Credentials = this._credentials; 
+ 
+ 
+             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) 
+             { 
+                 if (response.StatusCode != HttpStatusCode.OK) 
+                 { 
+                 } 
+                 else 
+                 { 
+                     Stream responseStream = response.GetResponseStream(); 
+                     if (responseStream != null) 
+                     { 
+                         try 
+                         { 
+                             StreamReader reader = new StreamReader(responseStream); 
+                             string originalText = reader.ReadToEnd();
+                             resultList.Add(originalText);
+
+                            /*  IEnumerable<XmlDocument> documents = GetXmlFromOriginalText(originalText); 
+
+
+                              foreach (XmlDocument document in documents) 
+                              { 
+                                  string resultString = ProcessDocument(document); 
+                                  if (resultString != null) 
+                                  { 
+                                      resultList.Add(resultString); 
+                                  } 
+                              } */
+                        } 
+                         catch (Exception) 
+                         { 
+                             //error in reading 
+                         } 
+                     } 
+                 } 
+             } 
+             return resultList; 
+         } 
+
+    }
+}
